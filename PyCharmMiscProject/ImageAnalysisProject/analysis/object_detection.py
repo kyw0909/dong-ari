d4a9a709c141530239f1de9c6cd8e3da04cc93f6
ISINNA473 <jin031655@gmail.com>
2025-05-20 22:57:21 +0900

    Add files via upload

---
+++ PyCharmMiscProject/ImageAnalysisProject/analysis/object_detection.py
@@ -0,0 +1,3 @@
+def detect_objects(image_path):
+    # 나중에 객체 탐지 코드 넣으면 됨
+    return {'사람': 1, '고양이': 2}
---
+++ PyCharmMiscProject/ImageAnalysisProject/analysis/ocr.py
@@ -0,0 +1,3 @@
+def extract_text(image_path):
+    # 나중에 실제 OCR 코드 넣으면 됨
+    return "예시 텍스트"
